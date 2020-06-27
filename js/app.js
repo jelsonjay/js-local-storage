0ec3b80bfb72f5f05d7c793e7dba1253887f337f
Jelson J <jelson20outlook.com>
2020-06-27 16:15:34 +0100

    update

--- js/app.js
+++ js/app.js
@@ -3,12 +3,14 @@ let user = (document.querySelector('#app').innerHTML = 'name');
 
 if (typeof Storage != 'undefined') {
 	if (localStorage.users) {
+		localStorage.users = Number(localStorage.users) + 1;
 	} else {
 		// localStorage.users
 		localStorage.setItem('users', 1);
-		localStorage.users = Number(localStorage.users) + 1;
 	}
-	document.write('users:' + localStorage.users);
+	let saved = 'Number of visitors:' + localStorage.users;
+	let b = document.querySelector('#app');
+	b.innerHTML = saved;
 } else {
-	document.write('This browser do not support local storage!');
+	document.write('This browser do not support!');
 }
